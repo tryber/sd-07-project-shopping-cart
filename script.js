d295b087b9397273644d1c3666525dfeb841a598
Bruno Nogueira <nugobruno@gmail.com>
2020-10-28 13:59:47 -0300

    Refazendo requisito 1 + codeclimate

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-window.onload = function onload() {
-  loadProductList();
-};
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -28,37 +24,34 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-//Baseado na aula do Vitor
-const loadProductList = () => {
-  const endpoint = "https://api.mercadolibre.com/sites/MLB/search?q=computador";
-  fetch(endpoint).then(response => response.json()).then(data => {
+// Baseado na aula do Vitor
+const createProductList = (searchFor) => {
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${searchFor}`;
+  fetch(endpoint)
+  .then(response => response.json())
+  .then((data) => {
     const items = document.querySelector('.items');
-    data.results.forEach((product) => {
 
+    data.results.forEach((product) => {
       const { id: sku, title: name, thumbnail: image } = product;
       const item = createProductItemElement({ sku, name, image });
       items.appendChild(item);
     });
   });
-}
+};
 
-//Criar uma lista de produtos através da API do Mercado Livre
-//"https://api.mercadolibre.com/sites/MLB/search?q=$QUERY"
-//$Query = valor da busca
-//sku = id
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+window.onload = function onload() {
+  createProductList('computador');
+};
